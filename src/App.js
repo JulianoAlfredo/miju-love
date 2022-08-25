2eae1c7d8a4c1898795ad5fd87c6de009e7be8f4
Juliano Alfredo <juliano.alfredo86@gmail.com>
2022-08-25 10:39:18 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='text-area'>
+        <p>
+          Oie mika, eu te amo
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
