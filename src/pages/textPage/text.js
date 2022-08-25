c0f018949b5e7787f34334e6cb31d2fb76df24dd
Juliano Alfredo <juliano.alfredo86@gmail.com>
2022-08-25 19:20:58 -0300

    CHANGE TEXT

--- src/pages/textPage/text.js
+++ src/pages/textPage/text.js
@@ -6,9 +6,15 @@ function Text(){
         <div className="App-mika">
             <div className='text-tomika'>
                 <div className="textmika-area">
-                    <p>LLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ligula lectus, blandit nec tortor nec, ultrices ullamcorper lorem. Etiam sit amet dui ut nulla tempor consectetur a sit amet dolor. Duis eget lectus vitae sem tempus auctor sed ut orci. Pellentesque at urna arcu. Nunc varius dolor quis leo fringilla bibendum. Donec sodales blandit nunc et sodales. Nunc et diam non felis rutrum ullamcorper. Sed a mi ac augue tempus porta eu id ex. Pellentesque id dapibus eros. Sed condimentum tellus ut lorem luctus ornare. Integer condimentum elementum imperdiet. Sed mi ante, lobortis vel dolor vel, fermentum pulvinar massa.
-                       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ligula lectus, blandit nec tortor nec, ultrices ullamcorper lorem. Etiam sit amet dui ut nulla tempor consectetur a sit amet dolor. Duis eget lectus vitae sem tempus auctor sed ut orci. Pellentesque at urna arcu. Nunc varius dolor quis leo fringilla bibendum.
-                       </p>
+                    <p>Oi vida, enfim 2 meses... Sei que a gente é muito mais que 2 meses, estamos juntos vai fazer 1 ano... Nesse 1 ano, nós vivemos de tudo, literalmente, de tudo...
+de uma forma calma, fomos atingindo níveis de intimidade que eu nunca tinha tido, você é uma pessoa que eu sempre gostei muito e sempre vou gostar, você é o amor da minha
+vida e eu não tenho vergonha de falar isso à ninguém. Só tenho a te agradecer por estar do meu lado sempre, me apoiando e me dizendo a melhor forma de agir para muitas
+coisas, você tem o seu jeitinho que, muitos podem te chamar de grossa, mas eu amo ele, é o jeito que falta em mim, alguém forte e sem medo de falar as coisas... As vezes
+você fala umas besteiras estressada, não só comigo, mas com terceiros KKKKKK e isso faz parte, é seu jeitinho fofo bravo de ser... Eu dei a sorte de conhecer a Mikaelle fofa,
+brava, responsável, enfim, quase todas kkkk, eu te amo muito, espero poder fazer um texto desse todos os meses da minha vida... Você sempre vai ser alguém que vou estar junto
+nós já superamos bastante coisas juntos, mas nada muito grave, pois somos muito responsáveis e quase não fazemos besteiras, e se fizermos, estamos juntos USAHDAUSH, tudo com
+você fica mais legal, bonito, divertido. Pode ser uma ida ao valão, se você estiver, com certeza eu vou rir, nós fomos feitos de piada, não conseguimos ficar sério 1 minuto. 
+Você tem sido minha razão de viver, te amo muito vida, você é muito especial para mim, pode não ter sido um texto fofo, mas, é um relato de como é viver ao seu lado.</p>
                 </div>
                 <button onClick={() =>{window.location.replace('https://miju-love.vercel.app/fotos')}}>Fotos</button>
                 
