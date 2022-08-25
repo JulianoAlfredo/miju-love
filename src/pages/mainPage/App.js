5436a3f8f34b852f49b493546a693f268f888a16
Juliano Alfredo <juliano.alfredo86@gmail.com>
2022-08-25 18:23:44 -0300

    changing

--- src/pages/mainPage/App.js
+++ src/pages/mainPage/App.js
@@ -9,8 +9,8 @@ function App() {
           <p>Olá vida,<br></br> para onde você deseja ir</p>
         </div>
         <div className='buttons'>
-          <button onClick={() =>{window.location.replace('https://miju-love.vercel.app//texto')}}>Texto</button>
-          <button onClick={() =>{window.location.replace('https://miju-love.vercel.app//fotos')}}>Fotos</button>
+          <button onClick={() =>{window.location.replace('https://miju-love.vercel.app/texto')}}>Texto</button>
+          <button onClick={() =>{window.location.replace('https://miju-love.vercel.app/fotos')}}>Fotos</button>
         </div>
       </div>
     </div>
--- src/pages/textPage/text.js
+++ src/pages/textPage/text.js
@@ -10,7 +10,7 @@ function Text(){
                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ligula lectus, blandit nec tortor nec, ultrices ullamcorper lorem. Etiam sit amet dui ut nulla tempor consectetur a sit amet dolor. Duis eget lectus vitae sem tempus auctor sed ut orci. Pellentesque at urna arcu. Nunc varius dolor quis leo fringilla bibendum.
                        </p>
                 </div>
-                <button onClick={() =>{window.location.replace('https://miju-love.vercel.app//fotos')}}>Fotos</button>
+                <button onClick={() =>{window.location.replace('https://miju-love.vercel.app/fotos')}}>Fotos</button>
                 
             </div>
             
